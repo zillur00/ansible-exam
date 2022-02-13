848dec9aeeeab2ae0967d1e39a47e9d36f0b8782
ZILLUR RAHMAN <zillurcse@gmail.com>
2022-02-13 11:56:11 -0500

    Update adhoc.sh

--- adhoc.sh
+++ adhoc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible all  -m yum_repository -a 'name=EPEL description=RHEL8 baseurl=https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rmp gpgcheck=0 enabled=1' 
+ansible all  -m yum_repository -a 'name=EPEL description=RHEL8 baseurl=https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rmp gpgcheck=1 enabled=1' 
